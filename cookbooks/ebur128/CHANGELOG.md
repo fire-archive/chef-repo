2a2e9457ff5d16b6a3f0020f4f04aa52550c8ce3
K. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2014-06-29 09:37:04 -0700

    Build ebur128.

---
+++ cookbooks/ebur128/CHANGELOG.md
@@ -0,0 +1,13 @@
+ebur128 CHANGELOG
+=================
+
+This file is used to list changes made in each version of the ebur128 cookbook.
+
+0.1.0
+-----
+- [your_name] - Initial release of ebur128
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ cookbooks/ebur128/recipes/default.rb
@@ -0,0 +1,46 @@
+#
+# Cookbook Name:: ebur128
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+stamp = `date +%s`.strip
+build_working = "/tmp/ebur128-#{stamp}"
+include_recipe "homebrew"
+
+package 'ninja' do
+  action :install
+end
+
+package 'cmake' do
+  action :install
+end
+
+git "#{build_working}" do
+  repository "https://github.com/jiixyj/libebur128"
+  reference "master"
+end
+
+build = "#{build_working}/Build"
+
+directory "#{build}" do
+end
+
+script 'build' do
+  interpreter "bash"
+  user "fire"
+  cwd "#{build_working}/Build"
+  code <<-EOH
+  cmake -G Ninja #{build_working}
+  ninja
+  ninja install
+  EOH
+end
+
+directory "#{build_working}" do
+  recursive true
+  action :delete
+end
